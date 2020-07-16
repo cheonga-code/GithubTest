ce46563986abaf92383ef15dcfc7f40d5bb30aa6
cheonga-code <chengalongo@gmail.com>
2020-07-16 16:42:29 +0900

    2020-07-16 16:42 수정 김청아

---
+++ app/src/main/java/com/example/githubtest/Test1Activity.java
@@ -0,0 +1,17 @@
+package com.example.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+
+        //테스트중입니다 김청아
+
+    }
+}
