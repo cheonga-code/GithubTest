61fa0d051d5e181d93a5e3e3129bb027c01a6feb
username <webserver3315@naver.com>
2020-07-16 17:46:14 +0900

    신동준이 수정 했습니다.

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         //고은찬 안드로이드 스튜디오에서 청아누나꺼 열어서 코드 반영하기
         //version control에서 변경사항들 확인 가능
         //version control이 안보이는 사람들은 Help - findaction - version control 검색 > open
-        //변경 후 오른쪽 상당
 
+        //동준 수정.
     }
 
     @Override
