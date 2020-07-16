985ff016fd8285f0ea3b40572bfd6b35c23f2a98
goeunchan <goyouchan@naver.com>
2020-07-16 16:56:45 +0900

    참고용 주석을 달고 onresume()을 추가해봤어요

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -11,7 +11,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //고은찬 업데이트 테스트
-        //cjddj
-        //ㅔㅔㅔㅔㅔㅔ
+
+        //ㅔㅔㅔ
+
+        //고은찬 안드로이드 스튜디오에서 청아누나꺼 열어서 코드 반영하기
+        //version control에서 변경사항들 확인 가능
+        //version control이 안보이는 사람들은 Help - findaction - version control 검색 > open
+        //변경 후 오른쪽 상당
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //실험용 - 은찬
     }
 }
